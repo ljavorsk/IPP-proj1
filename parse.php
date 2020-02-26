60385642d6477d8617cde7c3f0fa3cf558e725b0
Lukas Javorsky <ljavorsk@redhat.com>
2020-02-26 20:47:25 +0100

    Completed project

--- parse.php
+++ parse.php
@@ -34,13 +34,279 @@ $instructions = array (
     "ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "STRI2INT", "CONCAT",
     "GETCHAR", "SETCHAR", "JUMPIFEQ", "JUMPIFNEQ");
 
-$uniqLabels = array();
+$uniqLabels = array();     // Array for unique labels for bonus solution
 
-    class calls {
-        function err_msg($message, $rc){
-            fwrite(STDERR, "Error: $message");
-            exit($rc);
+    // Prints the error to stderr
+    function err_msg($message, $rc){
+        fwrite(STDERR, "Error: $message\n");
+        exit($rc);
+    }
+
+    // Checks the variable validity
+    function checkVar($variable){
+        if (preg_match('/^[L,T,G]F@[a-zA-Z_\-\$&%*!?][0-9a-zA-Z_\-\$&%*!?]*$/',$variable))
+            return true;
+        else
+            return false;
+    }
+
+    function checkConst($constant){
+        if (preg_match('/^nil@nil$/', $constant));
+        else if (preg_match('/^bool@(true|false)$/', $constant));
+        else if (preg_match('/^int@\S+$/', $constant));
+        else if (preg_match('/^(string@([^\\\\\#\s]|(\\\\\d{3}))*)$/', $constant));
+        else{
+            return false;
         }
+        return true;
+    }
+
+    function checkLabel($label){
+        if (preg_match('/^[a-zA-Z_\-\$&%*!?][0-9a-zA-Z_\-\$&%*!?]*$/',$label))
+            return true;
+        else
+            return false;
+    }
+
+    function checkType($type){
+        if (preg_match('/^(int|string|bool)$/',$type))
+            return true;
+        else
+            return false;
+    }
+
+    function decideSyntaxOrWrongOpcodeErr($instruction, $unknownOpCodetErr, $instArgErr){
+        global $instructions, $wrongOpCodeErr, $lexOrSyntaxErr;
+        if (!in_array($instruction, $instructions))
+            err_msg($unknownOpCodetErr, $wrongOpCodeErr);
+        else
+            err_msg($instArgErr, $lexOrSyntaxErr);
+    }
+
+    function xmlCreateArgument($argOrder,$argType,$argValue){
+        global $xmlWrite;
+
+        xmlwriter_start_element($xmlWrite, "arg$argOrder");
+        xmlwriter_start_attribute($xmlWrite, 'type');
+        xmlwriter_text($xmlWrite, "$argType");
+        xmlwriter_end_attribute($xmlWrite);
+        xmlwriter_text($xmlWrite, "$argValue");
+        xmlwriter_end_element($xmlWrite);
+    }
+
+    // Decides what instruction is on the line, and check its syntax
+    function whatInstruction($lineCodeArray, $instruction){
+        global $numberOfInstructions, $numberOfLabels, $numberOfJumps, $uniqLabels, $lexOrSyntaxErr, $wrongOpCodeErr, $xmlWrite;
+
+        $instArgErr = "Number of arguments or argument syntax error.
+        (Instruction number: $numberOfInstructions, instruction name: $instruction).";
+
+        $unknownOpCodetErr = "Unknown operation code.
+        (Instruction number: $numberOfInstructions, instruction name: $instruction";
+
+        switch(sizeof($lineCodeArray)){
+            case 1:     // No argument instruction
+                switch($instruction){
+                    case "CREATEFRAME":
+                    case "PUSHFRAME":
+                    case "POPFRAME":
+                    case "BREAK":
+                        break;  // No action needed
+                    case "RETURN":
+                        $numberOfJumps++;   // Bonus
+                        break;
+                    default:
+                        decideSyntaxOrWrongOpcodeErr($instruction, $unknownOpCodetErr, $instArgErr);
+                }
+                break;
+            case 2:     // One argument
+                switch($instruction){
+                    case "DEFVAR":  // No action needed
+                    case "POPS":    // <var>
+                        if (!checkVar($lineCodeArray[1]))   // Not a variable
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+
+                        xmlCreateArgument(1,"var",$lineCodeArray[1]);
+                        break;
+                    case "PUSHS":
+                    case "WRITE":
+                    case "EXIT":
+                    case "DPRINT":
+                        if (!checkConst($lineCodeArray[1])){
+                            if (!checkVar($lineCodeArray[1]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(1,"var",$lineCodeArray[1]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[1]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            xmlCreateArgument(1,$typeOfArg,$valueOfArg);
+                        }
+                        break;
+                    case "LABEL":
+                        if (!checkLabel($lineCodeArray[1]))
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else {
+                            if (!in_array($lineCodeArray[1], $uniqLabels))  // Was the label counted already?
+                            {
+                                $uniqLabels[] = $lineCodeArray[1];  // This label is counted
+                                $numberOfLabels++;
+                            }
+                            xmlCreateArgument(1,"label",$lineCodeArray[1]);
+                        }
+                        break;
+                    case "CALL":
+                    case "JUMP":
+                        if (!checkLabel($lineCodeArray[1]))
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else {
+                            $numberOfJumps++;   // Bonus
+                            xmlCreateArgument(1,"label",$lineCodeArray[1]);
+                        }
+                        break;
+                    default:
+                        decideSyntaxOrWrongOpcodeErr($instruction, $unknownOpCodetErr, $instArgErr);
+                        
+                }
+                break;
+            case 3:     // Two arguments
+                switch($instruction){
+                    case "MOVE":
+                    case "INT2CHAR":
+                    case "STRLEN":
+                    case "TYPE":
+                    case "NOT":
+                        if (!checkVar($lineCodeArray[1]))
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else
+                            xmlCreateArgument(1,"var",$lineCodeArray[1]);
+                        
+                        if (!checkConst($lineCodeArray[2])){
+                            if (!checkVar($lineCodeArray[2]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(2,"var",$lineCodeArray[2]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[2]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            xmlCreateArgument(2,$typeOfArg,$valueOfArg);
+                        }
+                        break;
+                    case "READ":
+                        if (!checkVar($lineCodeArray[1]))
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else
+                            xmlCreateArgument(1,"var",$lineCodeArray[1]);
+                        
+                        if (!checkType($lineCodeArray[2]))
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else
+                            xmlCreateArgument(2,"type",$lineCodeArray[2]);
+                        
+                        break;
+                    default:
+                        decideSyntaxOrWrongOpcodeErr($instruction, $unknownOpCodetErr, $instArgErr);
+                }
+                break;
+            case 4:     // Three arguments
+                switch ($instruction){
+                    case "ADD":
+                    case "SUB":
+                    case "MUL":
+                    case "IDIV":
+                    case "LT":
+                    case "GT":
+                    case "EQ":
+                    case "AND":
+                    case "OR":
+                    case "STRI2INT":
+                    case "CONCAT":
+                    case "GETCHAR":
+                    case "SETCHAR":
+                        if (!checkVar($lineCodeArray[1]))       // First arg
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else
+                            xmlCreateArgument(1,"var",$lineCodeArray[1]);
+
+                        if (!checkConst($lineCodeArray[2])){    // Second arg
+                            if (!checkVar($lineCodeArray[2]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(2,"var",$lineCodeArray[2]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[2]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            xmlCreateArgument(2,$typeOfArg,$valueOfArg);
+                        }
+
+                        if (!checkConst($lineCodeArray[3])){    // Third arg
+                            if (!checkVar($lineCodeArray[3]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(3,"var",$lineCodeArray[3]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[3]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            xmlCreateArgument(3,$typeOfArg,$valueOfArg);
+                        }
+                        break;
+                    case "JUMPIFEQ":
+                    case "JUMPIFNEQ":
+                        if (!checkLabel($lineCodeArray[1]))     // First arg
+                            err_msg($instArgErr, $lexOrSyntaxErr);
+                        else {
+                            xmlCreateArgument(1,"label",$lineCodeArray[1]);
+                        }
+
+                        if (!checkConst($lineCodeArray[2])){    // Second arg
+                            if (!checkVar($lineCodeArray[2]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(2,"var",$lineCodeArray[2]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[2]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            xmlCreateArgument(2,$typeOfArg,$valueOfArg);
+                        }
+
+                        if (!checkConst($lineCodeArray[3])){    // Third arg
+                            if (!checkVar($lineCodeArray[3]))
+                                err_msg($instArgErr, $lexOrSyntaxErr);
+                            else
+                                xmlCreateArgument(3,"var",$lineCodeArray[3]);
+                        }
+                        else{
+                            $splitArg = explode('@', $lineCodeArray[3]);
+                            $typeOfArg = $splitArg[0];
+                            $valueOfArg = $splitArg[1];
+
+                            $numberOfJumps++;   // Bonus
+                            xmlCreateArgument(3,$typeOfArg,$valueOfArg);
+                        }
+                        break;
+                    default:
+                        decideSyntaxOrWrongOpcodeErr($instruction, $unknownOpCodetErr, $instArgErr);
+                }
+                break;
+            default:    // More than three arguments
+                err_msg($numOfArgErr, $lexOrSyntaxErr);
+        }
+        xmlwriter_end_element($xmlWrite); // End the instruction element
     }
 
     $stats = false;
@@ -48,7 +314,7 @@ $uniqLabels = array();
       "stats:",
       "loc",
       "comments",
-      "loc",
+      "labels",
       "jumps",
       "help"
     );
@@ -63,7 +329,7 @@ $uniqLabels = array();
         else if (preg_match( '/--jumps$/', $arg) );
         else if (preg_match( '/parse.php/', $arg));
         else {
-            calls::err_msg("Unknown option\n", $wrongParamErr);
+            err_msg("Unknown option", $wrongParamErr);
         }
     }
 
@@ -82,18 +348,18 @@ $uniqLabels = array();
             echo "      --stats=file [TYPE]    Shows stats of specific types\n";
             echo "                             If the type is not filled, file will be left blank\n";
             echo "TYPES:\n";
-            echo "      --loc   TODO\n";
-            echo "      --comments   TODO\n";
-            echo "      --labels   TODO\n";
-            echo "      --jumps   TODO\n";
+            echo "      --loc   Prints the number of instructions\n";
+            echo "      --comments   Prints the number of comments\n";
+            echo "      --labels   Prints the number of labels\n";
+            echo "      --jumps   Prints the number of jumps\n";
             exit(0);
         }
         else {
-            return calls::err_msg("--help cannot take any more arguments.\n", $wrongParamErr);
+            return err_msg("--help cannot take any more arguments.", $wrongParamErr);
         }
     }
 
-    if (count($argv) >= 2 && count($argv) < 7){
+    if (count($argv) >= 2){
         if (preg_grep('/--stats=.*/', $argv)){
             $stats = true;
             $file = $my_args["stats"];
@@ -120,40 +386,47 @@ $uniqLabels = array();
             }
 
             $statFile = fopen($file, "w");
-            if(!$statFile){
-                calls::err_msg("File for statistics cannot be opened\n", $outputFileErr);
-            }
+            if(!$statFile)
+                err_msg("File for statistics cannot be opened", $outputFileErr);
         }
         else {
-            calls::err_msg("You're trying to specify stats without '--stats' option\n", $wrongParamErr);
+            err_msg("You're trying to specify stats without '--stats' option", $wrongParamErr);
         }
-    }
-    else {
-        calls::err_msg("Too many arguments\n", $wrongParamErr);
     }
 
     $input = fopen("php://stdin","r");
-    if ($input === false){
-        calls::err_msg("Failed to read from stdin\n", $inputFileErr);
+    if ($input === false)
+        err_msg("Failed to read from stdin", $inputFileErr);
+
+    // Comments and newlines at the beginning check
+    while (!feof($input)){
+        $currentLine = (fgets($input)); // Reads first lines from STDIN until header comes
+        $currentLine = trim($currentLine); // Trims whitespaces from beginning and the end of the line
+
+        if (strlen($currentLine) == 0){     // It was empty line
+            continue;
+        }
+        else if ($currentLine[0] == '#'){   // It was only comment
+            $numberOfComments++;
+            continue;
+        }
+        // First line of actual code
+        break;
     }
-
-    // Header check
-    $currentLine = (fgets($input)); // Reads first line from STDIN
-    $currentLine = trim($currentLine); // Trims whitespaces from beginning and the end of the line
+    
     $commentLine = explode('#', $currentLine);
-
-    if (sizeof($commentLine)) // Checks for comments
+    if (sizeof($commentLine) > 1) // Checks for comment after header
         $numberOfComments++;
 
+    // Header validity check
     $header = strtoupper(trim($commentLine[0]));
-    if (strcmp($header,".IPPCODE20") != 0) {    // Verify the header
-        calls::err_msg("Wrong header on input\n", $wrongHeaderErr);
-    }
+    if (strcmp($header,".IPPCODE20") != 0)    // Verify the header
+        err_msg("Wrong header on input", $wrongHeaderErr);
 
     // XML header
     $xmlWrite = xmlwriter_open_memory();
     xmlwriter_set_indent($xmlWrite, 1);
-    $res = xmlwriter_set_indent_string($xmlWrite, '   ');
+    xmlwriter_set_indent_string($xmlWrite, '   ');
 
     xmlwriter_start_document($xmlWrite, '1.0', 'UTF-8');
 
@@ -165,6 +438,41 @@ $uniqLabels = array();
     xmlwriter_text($xmlWrite, 'IPPcode20');
     xmlwriter_end_attribute($xmlWrite);
 
+    // While cycle for whole input
+    while (!feof($input)){
+        $currentLine = (fgets($input));
+        if (strcmp($currentLine, "\n") == 0 || $currentLine == "")     // Ignore empty lines
+            continue;
+
+        $commentLine = explode('#', $currentLine);
+        if (sizeof($commentLine) > 1){   // Checks for comments
+            $numberOfComments++;
+            if ($currentLine[0] == '#'){   // It was only comment
+                continue;
+            }
+            $currentLine = trim($commentLine[0]);     // Trims the comments
+        }
+        
+        $numberOfInstructions++;    // Order and also used for --loc option
+
+        $lineCodeArray = preg_split('/\s+/', $currentLine, 0, PREG_SPLIT_NO_EMPTY);  // Delete whitespaces
+
+        
+        $inst = strtoupper($lineCodeArray[0]);
+        if (!in_array($inst,$instructions))     // Check for valid instruction
+            err_msg("Unknown instruction", $wrongOpCodeErr);
+
+        xmlwriter_start_element($xmlWrite, 'instruction');
+        xmlwriter_start_attribute($xmlWrite, 'order');
+        xmlwriter_text($xmlWrite, "$numberOfInstructions");
+        xmlwriter_end_attribute($xmlWrite);
+
+        xmlwriter_start_attribute($xmlWrite, 'opcode');
+        xmlwriter_text($xmlWrite, "$inst");
+        xmlwriter_end_attribute($xmlWrite);
+        whatInstruction($lineCodeArray, $inst);
+
+    }
 
     // End of the XML document
     xmlwriter_end_element($xmlWrite);
